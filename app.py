f6b059a6d1011f3899ab87eff75c6d0dea771aaa
taeheelee90 <taehee.lee90@gmail.com>
2021-02-08 20:41:19 +0200

    Add for submit controller

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
 import os
 from flask import Flask 
 from flask import render_template
+from flask import request # for HTTP methods
+from flask import redirect
 from models import db
+
+from models import WebUser
 
 app = Flask(__name__)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+         #get data from form
+        userid = request.form.get('userid')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        re_password = request.form.get('re-password')
+
+        #validate: not null
+        if (userid and username and password and re_password) and password == re_password:
+            #Save data
+            webUser = WebUser()
+            webUser.userid = userid
+            webUser.username = username
+            webUser.password = password
+
+            db.session.add(webUser)
+            db.session.commit()
+            return redirect('/')
+    return render_template('registration.html')
+
 @app.route('/')
 def hello():
-    return render_template('registration.html')
+    return render_template('hello.html')
 
 # 지금까지는 flask run으로 실행했다면, 이제 python으로 실행하도록 설정
 if __name__ == "__main__":
